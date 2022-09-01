91dc0c22d1eedfb00dfdc2e3df2c7351b456514e
Marcos Gaius <87880897+MarcosGaius@users.noreply.github.com>
2022-09-01 11:37:17 -0400

    feat: added production requests

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -2,6 +2,8 @@ import { getProfileRequest } from "../../services/api";
 import {} from "./styles";
 
 const Landing = () => {
+  //DELETAR ISSO AQUI, TESTES APENAS
+
   const doReq = async () => {
     const retorno = await getProfileRequest();
     console.log("~ retorno", retorno);
