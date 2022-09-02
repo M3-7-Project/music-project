334e1b1522462e44097547eae0981bca4289c7de
Marcos Gaius <87880897+MarcosGaius@users.noreply.github.com>
2022-09-02 18:23:33 -0400

    fix: removed trycatch from requests

--- src/services/api.js
+++ src/services/api.js
@@ -6,147 +6,83 @@ const flowApi = axios.create({
 });
 
 export const registerRequest = async (registerData) => {
-  try {
-    const response = await flowApi.post("/register/", { ...registerData });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post("/register/", { ...registerData });
+  return response;
 };
 
 export const loginRequest = async (loginData) => {
-  try {
-    const response = await flowApi.post("/login/", { ...loginData });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post("/login/", { ...loginData });
+  return response;
 };
 
 export const createProfileRequest = async (profileData, token) => {
-  try {
-    const response = await flowApi.post("/profiles/", profileData, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post("/profiles/", profileData, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const getProfileRequest = async (id = "", params = {}) => {
-  try {
-    const response = await flowApi.get(`/profiles/${id}`, { params: params });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/profiles/${id}`, { params: params });
+  return response;
 };
 
 export const updateProfileRequest = async (id, token) => {
-  try {
-    const response = await flowApi.post(`/profiles/${id}`, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post(`/profiles/${id}`, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const createProductionRequest = async (productionData, token) => {
-  try {
-    const response = await flowApi.post(`/production/`, productionData, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post(`/production/`, productionData, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const getProductionRequest = async (id = "", params = {}) => {
-  try {
-    const response = await flowApi.get(`/production/${id}`, { params: params });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/production/${id}`, { params: params });
+  return response;
 };
 
 export const updateProductionRequest = async (id, updateData, token) => {
-  try {
-    const response = await flowApi.patch(`/production/${id}`, updateData, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.patch(`/production/${id}`, updateData, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const deleteProductionRequest = async (id, token) => {
-  try {
-    const response = await flowApi.delete(`/production/${id}`, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.delete(`/production/${id}`, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const createCommentRequest = async (commentData, token) => {
-  try {
-    const response = await flowApi.post(`/comments/`, commentData, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post(`/comments/`, commentData, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const createVoteRequest = async (voteData, token) => {
-  try {
-    const response = await flowApi.post(`/score/`, voteData, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.post(`/score/`, voteData, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const getCommentRequest = async (id = "") => {
-  try {
-    const response = await flowApi.get(`/comments/${id}`);
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/comments/${id}`);
+  return response;
 };
 
 export const getVoteRequest = async (id = "") => {
-  try {
-    const response = await flowApi.get(`/score/${id}`);
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/score/${id}`);
+  return response;
 };
 
 export const deleteCommentRequest = async (id, token) => {
-  try {
-    const response = await flowApi.delete(`/comments/${id}`, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.delete(`/comments/${id}`, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const deleteVoteRequest = async (id, token) => {
-  try {
-    const response = await flowApi.delete(`/score/${id}`, { headers: { Authorization: `Bearer ${token}` } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.delete(`/score/${id}`, { headers: { Authorization: `Bearer ${token}` } });
+  return response;
 };
 
 export const getAllProfilesByTypeRequest = async (type) => {
-  try {
-    const response = await flowApi.get(`/profiles/`, { params: { type: type } });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/profiles/`, { params: { type: type } });
+  return response;
 };
 
 export const getProductionWithStatsRequest = async (id = "") => {
@@ -154,12 +90,8 @@ export const getProductionWithStatsRequest = async (id = "") => {
   params.append("_embed", "comments");
   params.append("_embed", "score");
 
-  try {
-    const response = await flowApi.get(`/production/${id}`, { params: params });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/production/${id}`, { params: params });
+  return response;
 };
 
 export const getProfilesWithProductionsRequest = async (id) => {
@@ -167,12 +99,8 @@ export const getProfilesWithProductionsRequest = async (id) => {
   params.append("_embed", "production");
   params.append("userId", id);
 
-  try {
-    const response = await flowApi.get(`/profiles/`, { params: params });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/profiles/`, { params: params });
+  return response;
 };
 
 export const getProfilesWithInteractionsRequest = async (id) => {
@@ -181,10 +109,6 @@ export const getProfilesWithInteractionsRequest = async (id) => {
   params.append("_embed", "score");
   params.append("userId", id);
 
-  try {
-    const response = await flowApi.get(`/profiles/`, { params: params });
-    return response;
-  } catch (error) {
-    return error;
-  }
+  const response = await flowApi.get(`/profiles/`, { params: params });
+  return response;
 };
