451bd2adf9aead3c22c8e87bd805ab24af2109f1
Marcos Gaius <87880897+MarcosGaius@users.noreply.github.com>
2022-09-02 09:00:16 -0400

    feat: added protected routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,33 @@
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Landing from '../pages/Landing';
-import ProducerPage from '../pages/ProducerPage';
-import Production from '../pages/Production';
-import Dashboard from '../pages/Dashboard';
-import { Navigate, Routes, Route } from "react-router-dom"
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Landing from "../pages/Landing";
+import ProducerPage from "../pages/ProducerPage";
+import Production from "../pages/Production";
+import Dashboard from "../pages/Dashboard";
+import { Navigate, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const RoutesMain = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Landing />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/producer-login' element={<Login />} />
-            <Route path='/user-register' element={<Register />} />
-            <Route path='/producer/:id' element={<ProducerPage />} />
-            <Route path='/album/:id' element={<Production />} />
-            <Route path='/music/:id' element={<Production />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='*' element={<Navigate to='/landing' />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/producer" element={<ProducerPage />}>
+          <Route path=":id" element={<ProducerPage />} />
+        </Route>
+        <Route path="/album" element={<Production />}>
+          <Route path=":id" element={<Production />} />
+        </Route>
+        <Route path="/music" element={<Production />}>
+          <Route path=":id" element={<Production />} />
+        </Route>
+      </Route>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
 
 export default RoutesMain;
